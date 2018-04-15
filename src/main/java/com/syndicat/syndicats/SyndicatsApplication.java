35e4876bcf528d6f9f6e2de877acd8abfeccff63
Haykel Maaoui <haykel.maaoui@gmail.com>
2018-04-16 00:37:12 +0200

    upload

---
+++ src/main/java/com/syndicat/syndicats/SyndicatsApplication.java
@@ -0,0 +1,12 @@
+package com.syndicat.syndicats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyndicatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyndicatsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/syndicat/syndicats/SyndicatsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syndicat.syndicats;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SyndicatsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
